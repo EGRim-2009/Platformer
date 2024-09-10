30cb43ddfdb4edbd5b17c66973c832d5e665cbed
hucu314 <hcumpean@ftstudent.org>
2024-09-10 14:03:37 -0400

    Fixed it
    Fixed it for one of the players just have to fix it for the other.

--- scrips/player.gd
+++ scrips/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var canMove = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -11,36 +12,39 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if playerVariables.damage == true:
-		animated_sprite.play("damaged")
-		await animated_sprite.animation_finished
-		playerVariables.damage = false
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	#if playerVariables.damage == true:
+		#animated_sprite.play("damaged")
+		#playerVariables.damage = false
+	if canMove == true:
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("p1jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle jump.
+		if Input.is_action_just_pressed("p1jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	#Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("p1left", "p1right")
-	if direction>0:
-		animated_sprite.flip_h = false
-	elif direction<0:
-		animated_sprite.flip_h = true
-		
-	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
+		#Get the input direction: -1, 0, 1
+		var direction = Input.get_axis("p1left", "p1right")
+		if direction>0:
+			animated_sprite.flip_h = false
+		elif direction<0:
+			animated_sprite.flip_h = true
+			
+		# Play animations
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("run")
 		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-		
-	#apply movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+			animated_sprite.play("jump")
+			
+		#apply movement
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
+
+	
+	pass # Replace with function body.
--- scrips/player2.gd
+++ scrips/player2.gd
@@ -12,10 +12,11 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if playerVariables.damage == true:
+		playerVariables.canMove = false
 		animated_sprite.play("damaged")
-		await animated_sprite.animation_finished
 		playerVariables.damage = false
-	else:
+
+	if playerVariables.canMove == true:
 		if not is_on_floor():
 			velocity.y += gravity * delta
 
@@ -45,3 +46,6 @@ func _physics_process(delta):
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 		move_and_slide()
+
+func _on_animated_sprite_2d_animation_finished():
+	playerVariables.canMove = true
